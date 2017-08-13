4a77e9906ff4f38a26e865607ad2db146699300b
ShizukuIchi <eddy540511@yahoo.com.tw>
2017-08-14 03:57:42 +0800

    add url argv

--- getM3u8.py
+++ getM3u8.py
@@ -2,9 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 
-m3u8Name = sys.argv[1]
+m3u8Name = sys.argv[2]
+url = sys.argv[1]
 
-res = requests.get('https://avgle.com/video/45447/27%E7%A8%AE%E9%A1%9E%E3%81%AE%E3%83%91%E3%82%A4%E3%82%BA%E3%83%AA%E3%81%A7%E3%82%A4%E3%82%AF%E3%81%A3-%E3%81%8A%E3%81%A3%E3%81%B1%E3%81%84%E5%A4%A7%E4%B9%B1%E4%BA%A4-%E7%BE%8E%E7%AB%B9%E3%81%99%E3%81%9A-miae-051')
+res = requests.get(url)
 
 soup = BeautifulSoup(res.text,"html.parser")
 m3u8Url = str(soup.source['src'])
